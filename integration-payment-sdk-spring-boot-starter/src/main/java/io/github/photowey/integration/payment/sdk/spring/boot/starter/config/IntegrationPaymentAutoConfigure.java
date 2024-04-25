edb7ce87bec6d25c784ae564cf02985efc940846
photowey <photowey@gmail.com>
2024-04-26 01:35:07 +0800

    feat: Add project multi-modules.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ integration-payment-sdk-spring-boot-starter/src/main/java/io/github/photowey/integration/payment/sdk/spring/boot/starter/config/IntegrationPaymentAutoConfigure.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.integration.payment.sdk.spring.boot.starter.config;
+
+import io.github.photowey.integration.payment.sdk.autoconfigure.config.IntegrationPaymentConfigure;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * {@code IntegrationPaymentAutoConfigure}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2024/04/26
+ */
+@Configuration
+@Import(IntegrationPaymentConfigure.class)
+public class IntegrationPaymentAutoConfigure {
+}
